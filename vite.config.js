dfc6235a9a0338e2bebb1365b37ca3294154f0ef
abue.ammar <abue.ammar@digigate360.com>
2024-01-10 10:15:28 +0600

    vite config change

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import react from "@vitejs/plugin-react-swc";
+import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    // this ensures that the browser opens upon server start
+    open: true,
+    // this sets a default port to 3000
+    port: 3000,
+  },
+  preview: {
+    port: 3000,
+  },
+});
