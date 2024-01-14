157aa1f186402be1091d32e49495a1d5794618d2
abue.ammar <abue.ammar@digigate360.com>
2024-01-14 16:39:39 +0600

    site logo added

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -15,13 +15,14 @@ const Header = () => {
               strokeLinejoin="round"
             >
               <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-              <path d="M10 20.777a8.942 8.942 0 0 1 -2.48 -.969" />
-              <path d="M14 3.223a9.003 9.003 0 0 1 0 17.554" />
-              <path d="M4.579 17.093a8.961 8.961 0 0 1 -1.227 -2.592" />
-              <path d="M3.124 10.5c.16 -.95 .468 -1.85 .9 -2.675l.169 -.305" />
-              <path d="M6.907 4.579a8.954 8.954 0 0 1 3.093 -1.356" />
+              <path d="M15 8h.01" />
+              <path d="M11 21h-5a3 3 0 0 1 -3 -3v-12a3 3 0 0 1 3 -3h12a3 3 0 0 1 3 3v5.5" />
+              <path d="M3 16l5 -5c.928 -.893 2.072 -.893 3 0l2 2" />
+              <path d="M17.8 20.817l-2.172 1.138a.392 .392 0 0 1 -.568 -.41l.415 -2.411l-1.757 -1.707a.389 .389 0 0 1 .217 -.665l2.428 -.352l1.086 -2.193a.392 .392 0 0 1 .702 0l1.086 2.193l2.428 .352a.39 .39 0 0 1 .217 .665l-1.757 1.707l.414 2.41a.39 .39 0 0 1 -.567 .411l-2.172 -1.138z" />
             </svg>
-            <span className="font-bold sm:inline-block mt-1">image compressor</span>
+            <span className="font-bold sm:inline-block mt-1">
+              image compressor
+            </span>
           </a>
           <nav className="flex items-center gap-6 text-sm">
             {/* Navigation links */}
